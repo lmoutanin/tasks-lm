e21435591dd3759918479f5f530b888662eeeb87
Moutanin Luc <luc.moutanin@gmail.com>
2025-03-20 11:17:08 +0100

    first commit

---
+++ app/src/main/java/com/example/tasks_lm/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.tasks_lm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tasks_lm.ui.theme.TaskslmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TaskslmTheme {
+               Surface (modifier = Modifier.fillMaxSize()) {
+                   GreetingImage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingImage( modifier: Modifier = Modifier) {
+      Column( modifier.fillMaxSize(),
+          verticalArrangement = Arrangement.Center,
+         horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+
+      Image(
+            painter = painterResource(R.drawable.ic_task_completed),
+            contentDescription = null,
+          modifier.align(Alignment.CenterHorizontally)
+      )
+          Text(
+              text = "All tasks completed",
+              modifier.padding(top = 24.dp, bottom = 8.dp),
+              fontWeight = FontWeight.Bold
+          )
+
+          Text(
+              text = "Nice work!",
+              fontSize = 16.sp
+          )
+  }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TaskslmTheme {
+        GreetingImage()
+    }
+}
